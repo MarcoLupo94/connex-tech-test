9113b1fd009195c75218e44e76a377db84c66501
MarcoLupo94 <lupo.marco94@gmail.com>
2022-12-14 14:30:55 +0000

    Created ApiService and utility function Fetcher)

---
+++ client/src/utils/fetcher.ts
@@ -0,0 +1,25 @@
+interface Response {
+  error: string | null;
+  data: {};
+}
+
+export const fetcher = async (
+  url: string,
+  mode: string,
+  token?: string
+): Promise<Response> => {
+  try {
+    const response = await fetch(url, {
+      method: mode,
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    });
+    const json = await response.json();
+    return { error: null, data: json };
+  } catch (e: any) {
+    return { error: e, data: {} };
+  }
+};
